1eaf35e28ea58c57a518632c4af22e2a18172f22
Paolo Bueno <paolohaji@gmail.com>
2017-02-07 16:18:49 -0200

    add logging for listing forms calls

--- application.js
+++ application.js
@@ -1,43 +1,24 @@
-var mbaasApi = require('fh-mbaas-api');
+var $fh = require('fh-mbaas-api');
 var express = require('express');
-var mbaasExpress = mbaasApi.mbaasExpress();
-var cors = require('cors');
+var mbaasExpress = $fh.mbaasExpress();
+var Promise = require('bluebird');
+var forms = Promise.promisifyAll($fh.forms);
+var _ = require('lodash');
 
 // list the endpoints which you want to make securable here
-var securableEndpoints;
-securableEndpoints = ['/hello'];
-
 var app = express();
-
-// Enable CORS for all requests
-app.use(cors());
-
-// Note: the order which we add middleware to Express here is important!
-app.use('/sys', mbaasExpress.sys(securableEndpoints));
-app.use('/mbaas', mbaasExpress.mbaas);
-
-/* uncomment this code if you want to use $fh.auth in the app preview
- * localAuth is only used for local development. 
- * If the app is deployed on the platform, 
- * this function will be ignored and the request will be forwarded 
- * to the platform to perform authentication.
-
-app.use('/box', mbaasExpress.auth({localAuth: function(req, cb){
-  return cb(null, {status:401, body: {"message": "bad request"}});
-}}));
-
-or
-
-app.use('/box', mbaasExpress.core({localAuth: {status:401, body: {"message": "not authorised”}}}));
-*/
-
-// allow serving of static files from the public directory
-app.use(express.static(__dirname + '/public'));
 
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
-app.use('/hello', require('./lib/hello.js')());
+forms.getFormsAsync({}).tap(console.log)
+.then(function(forms) {
+  return {
+    formids: _.map(forms.forms, '_id')
+  };
+})
+.then(forms.getPopulatedFormListAsync).tap(console.log)
+.catch(console.error);
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
@@ -45,5 +26,5 @@ app.use(mbaasExpress.errorHandler());
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port); 
+  console.log("App started at: " + new Date() + " on port: " + port);
 });
